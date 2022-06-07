ecfc69b2de4505aa34b202b773f8efd6d6abd53e
fahim-titan <alfahim027@gmail.com>
2022-06-06 21:13:06 -0600

    feat: add local strategy to login route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -13,5 +15,11 @@ export class AppController {
   @Get('/hello')
   helloRoute(): string {
     return 'oh lala la AD';
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { StocksModule } from './stocks/stocks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, StocksModule],
+  imports: [UsersModule, StocksModule, AuthModule],
 })
 export class AppModule {}
